2aaf0dcd8c933f252072a8399f4561f73493ca62
chengxuyuanlm <61463297+chengxuyuanlm@users.noreply.github.com>
2023-11-24 12:58:29 +0800

    Update Table.py

--- dbTool/Table.py
+++ dbTool/Table.py
@@ -1,7 +1,7 @@
 import argparse
 import pymysql
 from tabulate import tabulate
-from config import DATABASE_CONFIG
+from .config import DATABASE_CONFIG
 
 def connect_to_database(database_name):
     config = DATABASE_CONFIG.copy()
