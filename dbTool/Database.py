625faf3adef651d8f8d7275e7c3145c7f1e073af
chengxuyuanlm <61463297+chengxuyuanlm@users.noreply.github.com>
2023-11-24 18:09:08 +0800

    Update Database.py

--- dbTool/Database.py
+++ dbTool/Database.py
@@ -11,17 +11,11 @@ def connect_to_database(database_name=None):
 def get_all_databases(connection):
     try:
         with connection.cursor() as cursor:
-            # 获取所有数据库的查询
             sql_query = 'SHOW DATABASES;'
             cursor.execute(sql_query)
-
-            # 获取查询结果
             result = cursor.fetchall()
-
             return result
-
     except Exception as e:
-        # 处理异常
         print(f"Error: {e}")
         return None
 
@@ -30,23 +24,12 @@ def display_databases(databases):
         print(tabulate(databases, headers=['Databases'], tablefmt='grid'))
 
 def main():
-    # 创建参数解析器
     parser = argparse.ArgumentParser(description='Database Query Tool')
-
-    # 解析命令行参数
     args = parser.parse_args()
-
-    # 建立数据库连接
     connection = connect_to_database()
-
     if connection:
-        # 获取所有数据库
         databases = get_all_databases(connection)
-
-        # 显示结果
         display_databases(databases)
-
-        # 关闭数据库连接
         connection.close()
 
 if __name__ == "__main__":
