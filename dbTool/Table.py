f32dbab05e5884ea34baf27afab94ed7d662e35f
chengxuyuanlm <61463297+chengxuyuanlm@users.noreply.github.com>
2023-11-24 16:58:25 +0800

    Update Table.py

--- dbTool/Table.py
+++ dbTool/Table.py
@@ -1,6 +1,6 @@
+import csv
 import argparse
 import pymysql
-from tabulate import tabulate
 from .config import DATABASE_CONFIG
 
 def connect_to_database(database_name):
@@ -8,35 +8,43 @@ def connect_to_database(database_name):
     config['database'] = database_name
     return pymysql.connect(**config)
 
-def get_all_tables(connection):
+def update_data(connection, table, filepath, unique_column):
     try:
         with connection.cursor() as cursor:
-            # 获取所有表的查询
-            sql_query = 'SHOW TABLES;'
-            cursor.execute(sql_query)
+            # 打开 CSV 文件并读取数据
+            with open(filepath, 'r') as csv_file:
+                csv_reader = csv.DictReader(csv_file)
+                
+                # 执行更新操作
+                for row in csv_reader:
+                    # 获取记录的 ID
+                    record_id = row[unique_column]
+                    
+                    # 构建 SET 子句
+                    set_clause = ', '.join([f"`{column}` = '{value}'" for column, value in row.items() if column != unique_column])
+                    
+                    # 构建 UPDATE 语句
+                    sql_query = f"UPDATE `{table}` SET {set_clause} WHERE `{unique_column}` = '{record_id}';"
+                    cursor.execute(sql_query)
 
-            # 获取查询结果
-            result = cursor.fetchall()
+            # 提交事务
+            connection.commit()
 
-            return result
+        print(f"Data updated in `{table}` from {filepath} using `{unique_column}` as the identifier")
 
     except Exception as e:
-        # 处理异常
-        print(f"Error: {e}")
-        return None
-
-def display_tables(tables):
-    if tables:
-        print(tabulate(tables, headers=['Tables'], tablefmt='grid'))
-
-# get_tables_script.py
+        print(f"Error updating data: {e}")
 
 def main():
+
     # 创建参数解析器
-    parser = argparse.ArgumentParser(description='Database Query Tool')
+    parser = argparse.ArgumentParser(description='Database Update Tool')
 
     # 添加命令行参数
     parser.add_argument('-database', type=str, help='Database name', required=True)
+    parser.add_argument('-table', type=str, help='Table name', required=True)
+    parser.add_argument('-filepath', type=str, help='Filepath with data to update', required=True)
+    parser.add_argument('-unique_column', type=str, help='Column name for the identifier', required=True)
 
     # 解析命令行参数
     args = parser.parse_args()
@@ -45,44 +53,11 @@ def main():
     connection = connect_to_database(args.database)
 
     if connection:
-        # 获取所有表
-        tables = get_all_tables(connection)
-
-        # 显示结果
-        display_tables(tables)
+        # 更新数据
+        update_data(connection, args.table, args.filepath, args.unique_column)
 
         # 关闭数据库连接
         connection.close()
 
 if __name__ == "__main__":
     main()
-
-# def main():
-#     # 创建参数解析器
-#     parser = argparse.ArgumentParser(description='Database Query Tool')
-
-#     # 添加命令行参数
-#     parser.add_argument('-host', type=str, help='Database host', default='localhost')
-#     parser.add_argument('-port', type=int, help='Database port', default=3306)
-#     parser.add_argument('-user', type=str, help='Database user', default='mingl')
-#     parser.add_argument('-password', type=str, help='Database password', default="Asd081312+")
-#     parser.add_argument('-database', type=str, help='Database name')
-
-#     # 解析命令行参数
-#     args = parser.parse_args()
-
-#     # 建立数据库连接
-#     connection = connect_to_database(args)
-
-#     if connection:
-#         # 获取所有表
-#         tables = get_all_tables(connection)
-
-#         # 显示结果
-#         display_tables(tables)
-
-#         # 关闭数据库连接
-#         connection.close()
-
-# if __name__ == "__main__":
-#     main()
