ba7e15914bc9a8958707147d259b4e2176bedd31
Super-Detective <61463297+Super-Detective@users.noreply.github.com>
2023-11-30 16:33:23 +0800

    Update Table.py

--- dbTool/Table.py
+++ dbTool/Table.py
@@ -2,6 +2,15 @@ import argparse
 import pymysql
 from tabulate import tabulate
 from .config import DATABASE_CONFIG
+from configparser import ConfigParser
+
+# 加载配置文件 config.ini
+config = ConfigParser()
+config.read('config.ini')
+
+# 从配置文件设置默认值
+DEFAULT_DATABASE = config.get('DEFAULT', 'DATABASE')
+
 
 def connect_to_database(database_name):
     config = DATABASE_CONFIG.copy()
@@ -11,14 +20,17 @@ def connect_to_database(database_name):
 def get_all_tables(connection):
     try:
         with connection.cursor() as cursor:
+            # 获取所有表的查询
             sql_query = 'SHOW TABLES;'
             cursor.execute(sql_query)
 
+            # 获取查询结果
             result = cursor.fetchall()
 
             return result
 
     except Exception as e:
+        # 处理异常
         print(f"Error: {e}")
         return None
 
@@ -29,19 +41,30 @@ def display_tables(tables):
 # get_tables_script.py
 
 def main():
+    # 创建参数解析器
     parser = argparse.ArgumentParser(description='Database Query Tool')
 
-    parser.add_argument('-database', type=str, help='Database name', required=True)
+    # 添加命令行参数
+    parser.add_argument('-database', type=str, help='Database name')
 
+    # 解析命令行参数
     args = parser.parse_args()
 
+    # 如果未提供数据库和表，使用默认值
+    if not args.database:
+        args.database = DEFAULT_DATABASE
+
+    # 建立数据库连接
     connection = connect_to_database(args.database)
 
     if connection:
+        # 获取所有表
         tables = get_all_tables(connection)
 
+        # 显示结果
         display_tables(tables)
 
+        # 关闭数据库连接
         connection.close()
 
 if __name__ == "__main__":
