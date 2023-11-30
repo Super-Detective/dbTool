7bfc51500a6f38e82097494c8f323fb73a372235
Super-Detective <61463297+Super-Detective@users.noreply.github.com>
2023-11-30 16:30:57 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dbTool',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     install_requires=[
         'pymysql',
@@ -18,20 +18,20 @@ setup(
             'Count = dbTool.Count:main',
         ],
     },
-    author='Ming',
+    package_data={
+        '': ['config.ini'],
+    },
+    include_package_data=True,
+    author='Ming L',
     author_email='email@example.com',
     description='A set of tools for interacting with databases',
     long_description='This package provides command-line tools for various database operations.',
-    url='https://github.com/chengxuyuanlm/dbTool',
+    url='https:https://github.com/Super-Detective/dbTool',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.11.4',
     ],
     keywords='database tool sql pymysql',
 )
